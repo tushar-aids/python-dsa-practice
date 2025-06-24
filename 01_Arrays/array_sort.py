8880be26d7665729d427a9e3eb1a6637ff0324d1
Tushar pawar <tusharpawar9699@gmail.com>
2025-06-24 15:11:33 +0530

    Create array_sort.py

---
+++ 01_Arrays/array_sort.py
@@ -0,0 +1,17 @@
+# Sort an array in ascending and descending order
+
+nums = [40, 10, 50, 30, 20]
+
+# Ascending sort
+asc = sorted(nums)
+print("Ascending order:", asc)
+
+# Descending sort
+desc = sorted(nums, reverse=True)
+print("Descending order:", desc)
+
+
+
+# output:
+# Ascending order: [10, 20, 30, 40, 50]
+# Descending order: [50, 40, 30, 20, 10]
