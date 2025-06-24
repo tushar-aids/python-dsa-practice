dd3796f67a2426516907219439280b77c99f9c9f
Tushar pawar <tusharpawar9699@gmail.com>
2025-06-24 13:43:21 +0530

    Create array_sum.py

---
+++ 01_Arrays/array_sum.py
@@ -0,0 +1,15 @@
+# Calculate the sum of all elements in the array
+
+nums = [10, 20, 30, 40, 50]
+
+total = 0
+
+for num in nums:
+    total += num
+
+print("Sum of all elements:", total)
+
+
+
+# output:
+# Sum of all elements: 150
