bbc2bd6e38c78fd63e63c9a8aae6f0398a8b3c94
Tushar pawar <tusharpawar9699@gmail.com>
2025-06-24 15:23:22 +0530

    Create array_max_min_diff.py

---
+++ 01_Arrays/array_max_min_diff.py
@@ -0,0 +1,19 @@
+# Find difference between maximum and minimum elements in the array
+
+nums = [22, 5, 17, 39, 8, 26]
+
+maximum = max(nums)
+minimum = min(nums)
+
+diff = maximum - minimum
+
+print("Maximum element:", maximum)
+print("Minimum element:", minimum)
+print("Difference:", diff)
+
+
+
+output:
+Maximum element: 39
+Minimum element: 5
+Difference: 34
